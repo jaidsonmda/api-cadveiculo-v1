f41404896b8ca438a203df269df792c7f3c1b0d4
jaidsonmda <jaidson.mda@gmail.com>
2024-09-20 11:30:41 -0300

    Criação de estrutura para desenvolvimento

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Cria usuário, senha e banco de dados
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE ROLE teste;
+    CREATE USER teste WITH PASSWORD 'teste';
+    CREATE DATABASE teste;
+    GRANT ALL PRIVILEGES ON DATABASE teste TO teste;
+    GRANT teste TO teste;
+EOSQL
